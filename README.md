e7685a5851517a514088aff3b1c7de342cfdd91d
lf_shao <lf_shao@163.com>
2025-09-14 21:25:55 +0800

    v0.1.0

--- README.md
+++ README.md
@@ -1,21 +1,5 @@
-# GovOmniAgent
+# 一、使用说明
 
 
-```mermaid
-classDiagram
-    class users {
-        +id int PK
-        username varchar(50)
-        email varchar(100)
-        created_at timestamp
-    }
-    
-    class orders {
-        +order_id int PK
-        user_id int FK
-        order_date timestamp
-        amount decimal
-    }
-    
-    users "1" -- "many" orders
-```
+
+
---
+++ setup.py
@@ -0,0 +1,58 @@
+#!/usr/bin/env python3
+# -*- coding:utf-8 -*-
+# @License  ：(C)Copyright 2025, 数道智融科技
+# @Author   ：李锋
+# @Software ：PyCharm
+# @Date     ：2025/8/21 下午9:48
+# @Desc     ：
+from pathlib import Path
+
+from Cython.Build import cythonize
+from setuptools import Extension, find_namespace_packages, find_packages
+from setuptools import setup
+
+
+def _get_cython_extensions() -> list[Extension]:
+    """自动发现所有 .c 文件并为其创建 Cython 扩展对象"""
+    ext_modules = []
+
+    packages = find_packages(where=str(Path("src").resolve()))
+    relative_path = Path("src")
+
+    for package in packages:
+        package_path = relative_path / package.replace('.', '/')
+        for c_file in package_path.rglob("*.c"):
+            # 构建模块路径
+            rel_path = c_file.relative_to(relative_path)
+            module_name = str(rel_path.with_suffix('')).replace('/', '.')
+            ext_modules.append(
+                Extension(
+                    name=module_name,
+                    sources=[str(c_file)],
+                    extra_compile_args=[
+                        "-Wno-unreachable-code-fallthrough",
+                        "-Wno-unused-function",
+                        "-Wno-unreachable-code",
+                    ],
+                )
+            )
+    return ext_modules
+
+
+setup(
+    packages=find_namespace_packages(where="src"),
+    package_dir={"": "src"},
+    include_package_data=False,
+    ext_modules=cythonize(
+        _get_cython_extensions(),
+        compiler_directives={
+            'language_level': 3,
+            'annotation_typing': True,
+        },
+        # quiet=True,
+        annotate=True,
+        nthreads=4,  # 使用多线程编译
+    ),
+    # script_args=['build_ext'],
+    zip_safe=False,
+)
